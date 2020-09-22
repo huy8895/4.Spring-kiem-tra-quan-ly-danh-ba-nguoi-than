9be2642125dd7dcf898086a0bca350ed03058016
huy8895 <huy8895@gmail.com>
2020-09-22 15:23:36 +0700

    tao moi 2 model la user va category

--- src/main/java/com/quanlydanhba/model/Category.java
+++ src/main/java/com/quanlydanhba/model/Category.java
@@ -1,4 +1,49 @@
 package com.quanlydanhba.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    @OneToMany(targetEntity = User.class)
+    private List<User> users;
+
+    public Category() {
+    }
+
+    public Category( String name, List<User> users) {
+        this.name = name;
+        this.users = users;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
--- src/main/java/com/quanlydanhba/model/User.java
+++ src/main/java/com/quanlydanhba/model/User.java
@@ -1,4 +1,71 @@
 package com.quanlydanhba.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username;
+
+    private String address;
+
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public User() {
+    }
+
+    public User(String username, String address, String phoneNumber, Category category) {
+        this.username = username;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
